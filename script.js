85d969c474aef5cda113c486673303b5060dd1d8
SFujiN <d0n4lddanghuynh@gmail.com>
2023-03-04 19:56:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function createGrid(n) {
     // create an n x n grid
     for (let i=0; i<(n*n); i++) {
         let square = document.createElement('div');
-        square.classList.add('square');
+        square.classList.add('square-16');
         container.appendChild(square);
     }
 }
 
 createGrid(16);
 
-let grids = document.querySelectorAll('.square');
+let grids = document.querySelectorAll('.square-16');
 grids.forEach(grid => {
     grid.addEventListener('mouseover', (e) => {
         grid.classList.add('black');
