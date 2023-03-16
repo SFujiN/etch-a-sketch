d92ab6a397ed21c3d08881a3863e13b564bb7838
SFujiN <d0n4lddanghuynh@gmail.com>
2023-03-15 23:23:06 -0500

    Finish button resize
    - changed up the style rules
    - simplified some functions in script.js

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const container = document.querySelector('.container');
 const resizeBtn = document.querySelector('#resize');
 
+const SIDE = 960;
+
 function createGrid(n) {
     // create an n x n grid
     for (let i=0; i<(n*n); i++) {
         let square = document.createElement('div');
-        square.classList.add('square-16');
+        square.classList.add('square');
+
+        let dimensions = (1/n) * 100;
+        square.style.width = dimensions + '%';
+        square.style.height = dimensions + '%';
+
+        square.addEventListener('mouseover', (e) => {
+            square.classList.add('black');
+        });
         container.appendChild(square);
     }
 }
 
 createGrid(16);
 
-let grids = document.querySelectorAll('.square-16');
-grids.forEach(grid => {
-    grid.addEventListener('mouseover', (e) => {
-        grid.classList.add('black');
-    });
-});
-
 resizeBtn.addEventListener('click', () => {
-    prompt('Enter grid size: ');
-    delete grids;
-    console.log('click');
+    let size = parseInt(prompt('Enter grid size: '));
+    let grids = document.querySelectorAll('.square');
+    grids.forEach(grid => {
+        container.removeChild(grid);
+    });
+    createGrid(size);
 });
